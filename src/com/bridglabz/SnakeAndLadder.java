15f462006b883679bdf61c3a658fa095aef6d771
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-23 16:38:01 +0530

    Welcome to my Project

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridglabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Game");
+        int position=0;
+        System.out.println("Position of the player: "+ position);
     }
 }
