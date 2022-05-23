39a8f64a92dbfb2f8801f1aa95db8257d74671b2
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-23 17:32:25 +0530

    RollDie

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         System.out.println("Welcome to Game");
         int position=0;
         System.out.println("Position of the player: "+ position);
+        int die = (int)(Math.random()*6) + 1;
+        System.out.println("Number on die: "+ die);
     }
 }
