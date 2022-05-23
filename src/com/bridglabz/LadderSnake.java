669cdf312df9d5ec87234d0e139b479e4f7f95fc
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-23 20:51:12 +0530

    Winning 100

--- src/com/bridglabz/LadderSnake.java
+++ src/com/bridglabz/LadderSnake.java
@@ -1,6 +1,6 @@
 package com.bridglabz;
 
-public class LadderSnake {
+   public class Winning100 {
 
     public static final int  noPlay = 0;
     public static final int  ladder = 1;
@@ -8,23 +8,38 @@ public class LadderSnake {
 
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
+        int numberOfDie=0;
         int position = 0;
-        System.out.println("Position of the player: " + position);
-        int die = (int) (Math.random() * 6) + 1;
-        System.out.println("Number on die: " + die);
-        int ladderSnake = (int) (Math.random() * 3) + 1;
-        switch (ladderSnake) {
-            case noPlay:
-                System.out.println("noPlay is come");
-                break;
-            case ladder:
-                position = position + die;
-                System.out.println("Position of the player: " + position);
-                break;
-            case snake:
-                position = position + die;
-                System.out.println("Position of the player: " + position);
-                break;
+        System.out.println("Position of the player: "+ position);
+        while ( position <= winPosition) {
+            if(position<0) {
+                position = 0;
+            }
+            else {
+                int die = (int)(Math.random()*6) + 1;
+                System.out.println("Number on die: "+ die);
+                int ladderSnake = (int)(Math.random()*3) + 1;
+
+                switch(ladderSnake) {
+                    case noPlay:
+                        System.out.println("noPlay is come");
+                        break;
+                    case ladder:
+
+                        position=position + die;
+                        System.out.println("Position of the player: "+ position);
+                        numberOfDie++;
+                        break;
+                    case snake:
+
+                        position=position - die;
+
+                        System.out.println("Position of the player: "+ position);
+                        numberOfDie++;
+                        break;
+                }
+            }
+            System.out.println("Nummber of Die used: "+ numberOfDie);
         }
     }
 }
