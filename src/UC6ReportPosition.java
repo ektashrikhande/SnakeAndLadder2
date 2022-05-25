dec8fa640e1ae7a3f7277efb876316e7d5c47b95
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-25 20:36:58 +0530

    ReportPosition

---
+++ src/UC6ReportPosition.java
@@ -0,0 +1,53 @@
+public class UC6ReportPosition {
+        public static final int noPlay = 0;
+        public static final int ladder = 1;
+        public static final int snake = 2;
+        public static final int winPosition = 100;
+        public static final int positionCheck = 0;
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to snake and ladder game");
+            int numberOfDie = 0;
+            int position = 0;
+            System.out.println("Position of the player: " + position);
+
+            while (position < winPosition) {
+                if (position < 0) {
+                    position = 0;
+                } else {
+                    int die = (int) (Math.random() * 6) + 1;
+
+                    System.out.println("Number on die: " + die);
+                    int ladderSnake = (int) (Math.random() * 3) + 1;
+
+                    switch (ladderSnake) {
+                        case noPlay:
+                            System.out.println("noPlay is come");
+                            break;
+                        case ladder:
+                            if ((position + die) <= winPosition) {
+                                position = position + die;
+                            }
+                            System.out.println("Position of the player: " + position);
+                            numberOfDie++;
+                            break;
+                        case snake:
+
+                            position = position - die;
+                            if ((position - die) < positionCheck) {
+                                position = positionCheck;
+                            }
+
+                            System.out.println("Position of the player: " + position);
+                            numberOfDie++;
+                            break;
+                    }
+                }
+                System.out.println("Nummber of Die used: " + numberOfDie);
+            }
+        }
+    }
+
+
+
+
